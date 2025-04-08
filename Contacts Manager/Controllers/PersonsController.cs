15896698797b6d6673c6ccbd0f40bae6fea84569
Julia Grzesiewicz <julia.grzesiewicz@op.pl>
2025-04-08 23:26:23 +0200

    Add sort personList in Index View

--- Contacts Manager/Controllers/PersonsController.cs
+++ Contacts Manager/Controllers/PersonsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServiceContracts;
 using ServiceContracts.DTO;
+using ServiceContracts.Enums;
 
 namespace Contacts_Manager.Controllers
 {
@@ -18,26 +19,35 @@ namespace Contacts_Manager.Controllers
 
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index(string searchBy, string? searchString)
+        public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderEnum sortOrder = SortOrderEnum.ASC )
         {
+            //Search
             //Actual Name - Display name - 
             ViewBag.SearchFields = new Dictionary<string, string>()
             {
                 {nameof(PersonResponse.PersonName), "Person Name" },
                 {nameof(PersonResponse.Email), "Email" },
                 {nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+               
                 {nameof(PersonResponse.Gender), "Gender" },
                 {nameof(PersonResponse.CountryID), "Country" },
                 {nameof(PersonResponse.Address), "Address" }
             };
-
+            Console.WriteLine("fgh");
+            //Search
             List<PersonResponse> persons = _personsService.GetFilteredPersons(searchBy, searchString);
+            
             
             //Save search fields after searching
             ViewBag.CurrentSearchBy = searchBy;
             ViewBag.CurrentSearchString = searchString;
 
-            return View(persons); //Views/Persons/Index.cshtml
+            //Sort
+            List<PersonResponse> sortedPersons = _personsService.GetSortedPersons(persons, sortBy, sortOrder);
+            ViewBag.CurrentSortBy = sortBy;
+            ViewBag.CurremtSortOrder = sortOrder.ToString();
+
+            return View(sortedPersons); //Views/Persons/Index.cshtml
         }
     }
 }
